dbf3580cba044eff44538b2247d26c3fd724b9db
Haekal Hilmi <haekalhilmi10@gmail.com>
2024-12-17 17:03:45 +0800

    menambahkan styling

--- djangoweb/web/views.py
+++ djangoweb/web/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+  {
+    'author': 'haekalhilmi',
+    'title': 'Sejarah Organisasi Kemahasiswaan Informatika',
+    'content': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic libero assumenda odio atque eius veritatis minima nulla.',
+    'date_post': '17th December 2024'
+  },
+  {
+    'author': 'MMFRXX',
+    'title': 'Perampasan Ruang Kreasi Mahasiswa',
+    'content': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic libero assumenda odio atque eius veritatis minima nulla.',
+    'date_post': '20th December 2024'
+  }
+]
+
 def home(request):
-  return render(request, 'home.html')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'home.html', context)
 
 def about(request):
-  return render(request, 'about.html')
+  return render(request, 'about.html', {'title' : "Tentang Kami"})
