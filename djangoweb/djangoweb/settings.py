7cf66f5c79ae24af1b40f6720af43f55c9ff812e
Niqx <evansamuel90@gmail.com>
2024-12-18 17:17:02 +0800

    Add Create Post, Delete Post, Update Post, Profile, Edit Profile

--- djangoweb/djangoweb/settings.py
+++ djangoweb/djangoweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web.apps.WebConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 
@@ -123,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- djangoweb/web/urls.py
+++ djangoweb/web/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-  path('', views.home, name="home"),
-  path('about/', views.about, name="about")
+  #path('', views.home, name="home"),
+  path('about/', views.about, name="about"),
+  path('', PostListView.as_view(), name="home"),
+  path('post/new', PostCreateView.as_view(), name="new"),
+  path('post/<int:pk>/', PostDetailView.as_view(), name="detail"),
+  path('post/<int:pk>/update', PostUpdateView.as_view(), name="update"),
+  path('post/<int:pk>/delete', PostDeleteView.as_view(), name="delete")
+  
 ]
+
+ 
